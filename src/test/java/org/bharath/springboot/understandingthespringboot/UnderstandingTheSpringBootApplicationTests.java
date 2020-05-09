be072674cab4f89fb687e19846eb23246d0ad1a5
Bharath Kumar <bharathk494@gmail.com>
2020-05-09 21:23:30 +0530

    Initial Commit

---
+++ src/test/java/org/bharath/springboot/understandingthespringboot/UnderstandingTheSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bharath.springboot.understandingthespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderstandingTheSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
